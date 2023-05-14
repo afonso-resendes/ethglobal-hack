571f5ee3a92fca5f89c60e726c26118f3f6cb78d
Tomaz Silva <tomhoq2003@gmail.com>
2023-05-14 07:31:55 +0100

    final?

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,48 +1,9 @@
 import style from "./Posts.module.css";
-
-const post1 = {
-    title: "Post 1",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissimLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,",
-    expire_date: "2021-01-01",
-    reward: 100,
-    votes: 0,
-}
-
-const post2 = {
-    title: "Post 2",
-    content: "Lorem ipsum dolor sit amet, consecaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaatetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,",
-    expire_date: "2021-01-01",
-    reward: 100,
-    votes: 15,
-}
-
-const post3 = {
-    title: "Post 3",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscisssssssssssssssssssssssssssssssssssssssssssssssssng elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,",
-    expire_date: "2021-01-01",
-    reward: 100,
-    votes: 5,
-}
-
-const post4 = {
-    title: "Post 4",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,",
-    expire_date: "2021-01-01",
-    reward: 100,
-    votes: 10,
-}
-
-const post5 = {
-    title: "Post 5",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,",
-    expire_date: "2021-01-01",
-    reward: 100,
-    votes: 20,
-}
+import Post from "./PostsData.js"
 
 export default function Posts() {
 
-    const posts = [post1, post2, post3, post4, post5].map((post) => {
+    const posts = Post.map((post) => {
         return (
             <div className={style.post} key={post.title}>
                 <div className={style.left}>
